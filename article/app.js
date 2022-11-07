eb948e68fc3b8ec5abfb58a42a8bd59421b4bf56
Oly Rajak <59551153+olyrajak@users.noreply.github.com>
2022-11-08 01:03:28 +0530

    Update app.js

--- article/app.js
+++ article/app.js
@@ -32,17 +32,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
+
 app.use("/articles", articleRouter);
+
 app.use("/comments", commentrouter);
+
 
 
 app.use((req, res, next) => {
     res.status(404).send("Page Not Found");
 });
 
+
 app.use((err, req, res, next) => {
     res.send(err);
 });
+
 module.exports = app;
 
 
